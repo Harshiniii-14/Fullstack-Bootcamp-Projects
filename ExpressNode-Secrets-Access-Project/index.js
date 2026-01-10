1f08f2a7c856838dda87d2419bca07af218517d2
Harshiniii-14 <147309597+Harshiniii-14@users.noreply.github.com>
2026-01-10 18:49:46 +0530

    alt sol

--- ExpressNode-Secrets-Access-Project/index.js
+++ ExpressNode-Secrets-Access-Project/index.js
@@ -11,19 +11,38 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
+var userIsAuthorised = false;
+
 
 app.use(bodyParser.urlencoded({extended: true}));
+
+//creating a middleware for authorization instead of putting it in the post req
+
+function passwordCheck(req, res, next) {
+  const password = req.body["password"];
+  if (password === "ILoveProgramming") {
+    userIsAuthorised = true;
+  }
+  next();
+}
+app.use(passwordCheck);
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
 });
 
 app.post("/check", (req, res) => {
-    var password = req.body["password"];
-    console.log(password);
-    if(password === "ILoveProgramming") {
-        res.sendFile(__dirname + "/public/secret.html");
-    }
+    // var password = req.body["password"];
+    // console.log(password);
+    // if(password === "ILoveProgramming") {
+    //     res.sendFile(__dirname + "/public/secret.html");
+    // }
+    if (userIsAuthorised) {
+    res.sendFile(__dirname + "/public/secret.html");
+  } else {
+    res.sendFile(__dirname + "/public/index.html");
+    //Alternatively res.redirect("/");
+  }
 })
 
 app.listen(port, () => {
